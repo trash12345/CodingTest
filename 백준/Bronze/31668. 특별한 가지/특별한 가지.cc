12d773b5dbb40dc68e2c16468f51c2579992b444
Trash12345 <137850094+trash12345@users.noreply.github.com>
2024-09-04 10:08:44 +0900

    [Bronze IV] Title: 특별한 가지, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/31668. 특별한 가지/특별한 가지.cc
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(void)
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    int N, M, K;
+    cin >> N >> M >> K;
+
+    cout << M / N * K;
+
+    return 0;
+}

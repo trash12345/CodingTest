686b6a878e8b7e8f87c389c77bbd3f7bfacbd1ba
Trash12345 <137850094+trash12345@users.noreply.github.com>
2024-09-06 17:43:06 +0900

    [Silver I] Title: 예쁜 숫자, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/3152. 예쁜 숫자/README.md
@@ -0,0 +1,39 @@
+# [Silver I] 예쁜 숫자 - 3152 
+
+[문제 링크](https://www.acmicpc.net/problem/3152) 
+
+### 성능 요약
+
+메모리: 2020 KB, 시간: 0 ms
+
+### 분류
+
+애드 혹, 수학
+
+### 제출 일자
+
+2024년 9월 6일 17:42:58
+
+### 문제 설명
+
+<p>p는 2보다 큰 정수이다. 다음과 같은 규칙으로 무한 이진 트리의 각 노드에 정수인 숫자가 매겨진다.</p>
+
+<ul>
+	<li>루트 노드에는 1을 매긴다.</li>
+	<li>노드에 x가 매겨져 있다면 해당 노드의 왼쪽 자식 노드에는 p * x, 오른쪽 자식 노드에는 p * x + 1이 매겨진다.</li>
+</ul>
+
+<p>예를 들어 p = 3 일때 트리의 시작 부분은 다음과 같을 것이다.</p>
+
+<p style="text-align:center"><img src="https://onlinejudgeimages.s3-ap-northeast-1.amazonaws.com/userupload/insu_nym/20160121/20ac1a532aec950c639dcc70a7a6366e.png" style="height:305px; width:587px"></p>
+
+<p>어떤 숫자는 무한 이진 트리 내의 서로 다른 두 노드에 매겨진 두 숫자의 합으로 표현 할 수 있는 방법이 한 가지면 "예쁜 숫자"로 분류된다. 주어진 p로 만든 무한 이진 트리 내에서  n<sub>1</sub>, n<sub>2</sub>, n<sub>3</sub>, n<sub>4</sub>가 "예쁜"지 출력하는 프로그램을 작성하라.</p>
+
+### 입력 
+
+ <p>한 줄에 정수 p, n<sub>1</sub>, n<sub>2</sub>, n<sub>3</sub>, n<sub>4</sub>가 차례로 주어진다. (2 < p < 50, 0 < n<sub>1</sub> < 10<sup>18</sup>, 0 < n<sub>2</sub> < 10<sup>18</sup>, 0 < n<sub>3</sub> < 10<sup>18</sup>, 0 < n<sub>4</sub> < 10<sup>18</sup>)</p>
+
+### 출력 
+
+ <p>한 줄에 차례로 n<sub>1</sub>, n<sub>2</sub>, n<sub>3</sub>, n<sub>4</sub>가 예쁘면 1을, 아니면 0을 출력한다.</p>
+
---
+++ 백준/Silver/3152. 예쁜 숫자/예쁜 숫자.cc
@@ -0,0 +1,100 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+long long p;
+
+// p 진법에서 합의 개수가 1개로 유일한 경우
+// 1. 2인 경우 1+1
+// 2. 0인 경우 0+0
+// 그 외의 숫자 중 가능한 숫자는 1뿐 (1,0) or (0,1)
+
+bool solve(long long N)
+{
+    // p진법에서의 1과 2의 개수 이용
+    long long cntdiv_1 = 0, cntdiv_2 = 0;
+
+    while (N)
+    {
+        // div는 p진법에서의 각 자리 숫자
+        long long div = N % p;
+
+        if (div >= 3)
+        {
+            return false;
+        }
+        if (div == 1)
+        {
+            cntdiv_1++;
+        }
+        if (div == 2)
+        {
+            cntdiv_2++;
+        }
+
+        N /= p;
+    }
+
+    // 1000...01(p) 의 형태
+    // 1이 2개고 2가 0개인 경우 유일함
+    if (cntdiv_1 == 2 && cntdiv_2 == 0)
+    {
+        return true;
+    }
+
+    // 2가 있다면 다른 1자리만 1이어야 함
+    // 2개 이상이면 경우의 수가 2개 이상
+    if (cntdiv_1 == 1 && cntdiv_2 >= 1)
+    {
+        return true;
+    }
+
+    return false;
+}
+
+int main(void)
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    long long n1, n2, n3, n4;
+    cin >> p >> n1 >> n2 >> n3 >> n4;
+
+    if (solve(n1))
+    {
+        cout << "1 ";
+    }
+    else
+    {
+        cout << "0 ";
+    }
+
+    if (solve(n2))
+    {
+        cout << "1 ";
+    }
+    else
+    {
+        cout << "0 ";
+    }
+
+    if (solve(n3))
+    {
+        cout << "1 ";
+    }
+    else
+    {
+        cout << "0 ";
+    }
+
+    if (solve(n4))
+    {
+        cout << "1 ";
+    }
+    else
+    {
+        cout << "0 ";
+    }
+
+    return 0;
+}

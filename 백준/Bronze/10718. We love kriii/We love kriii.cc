07d27ca4bc2e6c6a59d15d98a3e520d1614d860d
Trash12345 <137850094+trash12345@users.noreply.github.com>
2024-09-08 21:52:27 +0900

    [Bronze V] Title: We love kriii, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10718. We love kriii/We love kriii.cc
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(void)
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    cout << "강한친구 대한육군\n";
+    cout << "강한친구 대한육군\n";
+
+    return 0;
+}

10f7beb85d4a13a20a3bb7c5a45d2953755c22ee
Trash12345 <137850094+trash12345@users.noreply.github.com>
2024-09-12 03:05:18 +0900

    [Bronze V] Title: 삼각형, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/29751. 삼각형/삼각형.cc
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(void)
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    int W, H;
+    cin >> W >> H;
+
+    cout << fixed;
+    cout.precision(1);
+
+    cout << W * H * double(0.5);
+
+    return 0;
+}

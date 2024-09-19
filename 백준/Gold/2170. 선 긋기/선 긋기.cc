9d367c0158749edb83b2e5790728483f03b010b3
Trash12345 <137850094+trash12345@users.noreply.github.com>
2024-09-19 11:22:53 +0900

    [Gold V] Title: 선 긋기, Time: 344 ms, Memory: 14440 KB -BaekjoonHub

---
+++ 백준/Gold/2170. 선 긋기/선 긋기.cc
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(void)
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    vector<pair<int, int>> V;
+
+    int N;
+    cin >> N;
+
+    for (int i = 0; i < N; i++)
+    {
+        int x, y;
+        cin >> x >> y;
+        V.push_back({x, y});
+    }
+
+    sort(V.begin(), V.end());
+
+    int s = V[0].first, e = V[0].second;
+    long long ans = 0;
+
+    for (int i = 0; i < N; i++)
+    {
+        if (e < V[i].first)
+        {
+            ans += e - s;
+            s = V[i].first;
+            e = V[i].second;
+        }
+        else if (e < V[i].second)
+        {
+            e = V[i].second;
+        }
+    }
+
+    ans += e - s;
+
+    cout << ans;
+
+    return 0;
+}

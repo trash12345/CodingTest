97bf6cef2a8af074661e326bcde5b228a2e459f8
Trash12345 <137850094+trash12345@users.noreply.github.com>
2024-09-23 01:39:14 +0900

    [Bronze IV] Title: Atrium, Time: 0 ms, Memory: 2032 KB -BaekjoonHub

---
+++ 백준/Bronze/20353. Atrium/Atrium.cc
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(void)
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    double a;
+    cin >> a;
+
+    cout << fixed;
+    cout.precision(15);
+
+    cout << sqrt(a) * 4;
+
+    return 0;
+}

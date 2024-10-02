714ce6e2117d9db6006dcfc9581529bfd7466a42
Trash12345 <137850094+trash12345@users.noreply.github.com>
2024-10-02 17:27:42 +0900

    [Gold IV] Title: 네트워크 연결, Time: 40 ms, Memory: 6800 KB -BaekjoonHub

---
+++ 백준/Gold/1922. 네트워크 연결/네트워크 연결.cc
@@ -0,0 +1,70 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+vector<pair<int, pair<int, int>>> edges;
+
+int parent[10001];
+
+int findParent(int x)
+{
+    if (parent[x] == x)
+    {
+        return x;
+    }
+    return parent[x] = findParent(parent[x]);
+}
+
+void uni(int x, int y)
+{
+    x = findParent(x);
+    y = findParent(y);
+
+    parent[y] = x;
+}
+
+int main(void)
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    int V, E;
+    cin >> V >> E;
+
+    for (int i = 0; i < E; i++)
+    {
+        int A, B, C;
+        cin >> A >> B >> C;
+
+        edges.push_back({C, {A, B}});
+        edges.push_back({C, {B, A}});
+    }
+
+    for (int i = 0; i < 10001; i++)
+    {
+        parent[i] = i;
+    }
+
+    sort(edges.begin(), edges.end());
+
+    int sum = 0;
+
+    for (int i = 0; i < edges.size(); i++)
+    {
+        int cost = edges[i].first;
+        int cur = edges[i].second.first;
+        int nxt = edges[i].second.second;
+
+        if (findParent(cur) == findParent(nxt))
+        {
+            continue;
+        }
+
+        uni(cur, nxt);
+        sum += cost;
+    }
+
+    cout << sum;
+
+    return 0;
+}

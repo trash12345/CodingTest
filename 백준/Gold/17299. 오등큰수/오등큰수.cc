1dbcf34d69688dafa7e49169702d632ceb48d85b
Trash12345 <137850094+trash12345@users.noreply.github.com>
2024-10-05 02:15:54 +0900

    [Gold III] Title: 오등큰수, Time: 192 ms, Memory: 18020 KB -BaekjoonHub

---
+++ 백준/Gold/17299. 오등큰수/오등큰수.cc
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int ans[1000001];
+int cnt[1000001];
+
+int main(void)
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    int N;
+    cin >> N;
+
+    vector<int> V(N, 0);
+    stack<int> S;
+    fill(ans, ans + N, -1);
+
+    for (int i = 0; i < N; i++)
+    {
+        cin >> V[i];
+        cnt[V[i]]++;
+    }
+
+    for (int i = 0; i < N; i++)
+    {
+        while (!S.empty() && cnt[V[S.top()]] < cnt[V[i]])
+        {
+            ans[S.top()] = V[i];
+            S.pop();
+        }
+
+        S.push(i);
+    }
+
+    for (int i = 0; i < N; i++)
+    {
+        cout << ans[i] << " ";
+    }
+
+    return 0;
+}

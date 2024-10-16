4c320d655fa38e1a38d6a97460906fc64c715142
Trash12345 <137850094+trash12345@users.noreply.github.com>
2024-10-16 16:30:38 +0900

    [Platinum V] Title: 책 페이지, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Platinum/1019. 책 페이지/책 페이지.cc
+++ 백준/Platinum/1019. 책 페이지/책 페이지.cc
@@ -1,10 +1,11 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 
 using namespace std;
 
 long long cnt[10];
 
-int main(void) {
+int main(void)
+{
     ios::sync_with_stdio(0);
     cin.tie(0);
 
@@ -14,24 +15,28 @@ int main(void) {
 
     cin >> N;
 
-    while(N) {
-        int div = N%10;
+    while (N)
+    {
+        int div = N % 10;
         N /= 10;
 
-        for(int i=0; i<div; i++) {
-            cnt[i] += (N+1)*digit;
+        for (int i = 0; i < div; i++)
+        {
+            cnt[i] += (N + 1) * digit;
         }
-        for(int i=div+1; i<10; i++) {
-            cnt[i] += N*digit;
+        for (int i = div + 1; i < 10; i++)
+        {
+            cnt[i] += N * digit;
         }
-        cnt[div] += N*digit + tmp + 1;
+        cnt[div] += N * digit + tmp + 1;
         cnt[0] -= digit;
 
-        tmp += div*digit;
+        tmp += div * digit;
         digit *= 10;
     }
 
-    for(int i=0; i<10; i++) {
+    for (int i = 0; i < 10; i++)
+    {
         cout << cnt[i] << " ";
     }
 

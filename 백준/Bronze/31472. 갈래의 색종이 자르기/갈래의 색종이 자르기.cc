0651d7ac96e67c925e400e26bdbd7b1fc0a8cde3
Trash12345 <137850094+trash12345@users.noreply.github.com>
2024-10-17 17:40:19 +0900

    [Bronze III] Title: 갈래의 색종이 자르기, Time: 0 ms, Memory: 2032 KB -BaekjoonHub

---
+++ 백준/Bronze/31472. 갈래의 색종이 자르기/갈래의 색종이 자르기.cc
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(void)
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    int w;
+    cin >> w;
+
+    w *= 2;
+
+    cout << (int)sqrt(w) * 4;
+
+    return 0;
+}

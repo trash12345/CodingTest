3edd4a3f28b84c0d49783c722f642d0605240509
Trash12345 <137850094+trash12345@users.noreply.github.com>
2024-10-17 17:32:47 +0900

    [Bronze V] Title: 홍익대학교, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/16394. 홍익대학교/홍익대학교.cc
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(void)
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    int N;
+    cin >> N;
+
+    cout << N - 1946;
+
+    return 0;
+}

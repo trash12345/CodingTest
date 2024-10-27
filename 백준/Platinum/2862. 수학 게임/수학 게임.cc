cc9a11b52890ed9db70dd21ef1314192cac3bd77
Trash12345 <137850094+trash12345@users.noreply.github.com>
2024-10-28 02:14:21 +0900

    [Platinum I] Title: 수학 게임, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Platinum/2862. 수학 게임/수학 게임.cc
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+long long fibo[91];
+
+int main(void)
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    long long N;
+    cin >> N;
+
+    fibo[1] = 1;
+    fibo[2] = 1;
+
+    for (int i = 3; i <= 90; i++)
+    {
+        fibo[i] = fibo[i - 1] + fibo[i - 2];
+    }
+
+    for (int i = 90; i >= 1; i--)
+    {
+        if (fibo[i] == N)
+        {
+            break;
+        }
+
+        if (N > fibo[i])
+        {
+            N -= fibo[i];
+        }
+    }
+
+    cout << N;
+
+    return 0;
+}

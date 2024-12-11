0e92cef4dd3b24f083aa85cce8080fa3e8ce8370
Trash12345 <137850094+trash12345@users.noreply.github.com>
2024-12-12 02:36:52 +0900

    [Bronze V] Title: Can you add this?, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/7891. Can you add this？/Can you add this？.cc
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(void)
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    int T;
+    cin >> T;
+
+    while (T--)
+    {
+        int x, y;
+        cin >> x >> y;
+
+        cout << x + y << "\n";
+    }
+
+    return 0;
+}

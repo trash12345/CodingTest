b3674ff32a05c50ab440c306bb01aa77f211952f
Trash12345 <137850094+trash12345@users.noreply.github.com>
2024-12-12 02:57:04 +0900

    [Bronze V] Title: 身長 (Height), Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/24086. 身長 （Height）/身長 （Height）.cc
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(void)
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    int A, B;
+    cin >> A >> B;
+
+    cout << B - A;
+
+    return 0;
+}

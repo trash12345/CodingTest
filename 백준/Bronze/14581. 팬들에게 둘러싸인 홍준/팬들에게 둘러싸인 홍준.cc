0a86c64ebcfac329f9f7f9e46b1cd6b55fcf1c67
Trash12345 <137850094+trash12345@users.noreply.github.com>
2024-12-12 02:39:56 +0900

    [Bronze V] Title: 팬들에게 둘러싸인 홍준, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/14581. 팬들에게 둘러싸인 홍준/팬들에게 둘러싸인 홍준.cc
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(void)
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    string S;
+    cin >> S;
+
+    cout << ":fan::fan::fan:\n";
+    cout << ":fan::" << S << "::fan:\n";
+    cout << ":fan::fan::fan:";
+
+    return 0;
+}

5027d8d1e65ae7c71142f2649389866eb1f59fcb
Trash12345 <137850094+trash12345@users.noreply.github.com>
2024-12-12 10:45:16 +0900

    [Bronze V] Title: 火曜日 (Tuesday), Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/31611. 火曜日 （Tuesday）/火曜日 （Tuesday）.cc
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(void)
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    int X;
+    cin >> X;
+
+    if (X % 7 == 2)
+    {
+        cout << 1;
+    }
+    else
+    {
+        cout << 0;
+    }
+
+    return 0;
+}

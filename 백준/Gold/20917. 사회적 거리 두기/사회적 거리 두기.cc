838f0d2b3f8daf5e8d90eb27681dbcfd1bf5dca1
Trash12345 <137850094+trash12345@users.noreply.github.com>
2024-12-19 04:51:07 +0900

    [Gold IV] Title: 사회적 거리 두기, Time: 456 ms, Memory: 4192 KB -BaekjoonHub

---
+++ 백준/Gold/20917. 사회적 거리 두기/사회적 거리 두기.cc
@@ -0,0 +1,64 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(void)
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    int T;
+    cin >> T;
+
+    while (T--)
+    {
+        int N, C;
+        cin >> N >> C;
+
+        int ans = 0;
+
+        vector<int> V;
+
+        for (int i = 0; i < N; i++)
+        {
+            int x;
+            cin >> x;
+
+            V.push_back(x);
+        }
+
+        sort(V.begin(), V.end());
+
+        int s = 1, e = V[N - 1] - V[0];
+
+        while (s <= e)
+        {
+            int mid = (s + e) / 2;
+            int cnt = 1;
+            int pos = V[0];
+
+            for (int i = 1; i < N; i++)
+            {
+                if (V[i] - pos >= mid)
+                {
+                    cnt++;
+                    pos = V[i];
+                }
+            }
+
+            if (cnt >= C)
+            {
+                ans = mid;
+                s = mid + 1;
+            }
+            else
+            {
+                e = mid - 1;
+            }
+        }
+
+        cout << ans << "\n";
+    }
+
+    return 0;
+}
